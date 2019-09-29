ee2d4f4338ccb315f1dc1f2998df8d50816a4281
ashwani <ashwnaidubey9835@gmail.com>
2019-09-30 00:21:39 +0530

    second person updated

--- src/packageToLearnGit/dummypage2.java
+++ src/packageToLearnGit/dummypage2.java
@@ -5,6 +5,8 @@ public class dummypage2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is 1st output from dummypage2");
+		
+		System.out.println("this is 2d output from dummypage2");
 
 	}
 
