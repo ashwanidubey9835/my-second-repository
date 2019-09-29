61c42c529d8cc9c497a375eccd1196a68c5654d8
ashwani <ashwnaidubey9835@gmail.com>
2019-09-29 23:41:27 +0530

    doing first commit

---
+++ src/packageToLearnGit/dummypage2.java
@@ -0,0 +1,11 @@
+package packageToLearnGit;
+
+public class dummypage2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is 1st output from dummypage2");
+
+	}
+
+}
